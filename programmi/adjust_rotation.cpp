34e04be99ae28ace9bf4714df4256098805ff2fe
EttoreFavari <ettore.favari@unitn.it>
2021-04-14 11:45:32 +0200

    Aggiunto cropping immagine ruotata

--- programmi/adjust_rotation.cpp
+++ programmi/adjust_rotation.cpp
@@ -44,8 +44,10 @@ int main(int argc, char* argv[]) {
 
     if (GUI) {
         if (SHOWINPUT) {
+
             namedWindow(input, WINDOW_KEEPRATIO);
             imshow(input, img);
+            cout<<"input sizes "<<img.size()<<endl;
         }
     }
     Mat card_rotated = rotate_card(img);
@@ -53,6 +55,7 @@ int main(int argc, char* argv[]) {
         if (SHOWOUTPUT) {
             namedWindow(output, WINDOW_AUTOSIZE);
             imshow(output, card_rotated);
+            cout<<"output sizes"<<card_rotated.size()<<endl;
             waitKey(0);
         }
     }
@@ -62,9 +65,9 @@ int main(int argc, char* argv[]) {
 
 
 double lato(Point2f point1, Point2f point2) {
-    double x_2 = pow(point1.x-point2.x,2);
-    double y_2 = pow(point1.y-point2.y,2);
-    double ret = sqrt(x_2+y_2);
+    double x_2 = pow(point1.x - point2.x, 2);
+    double y_2 = pow(point1.y - point2.y, 2);
+    double ret = sqrt(x_2 + y_2);
     return ret;
 }
 
@@ -75,18 +78,15 @@ int area_rect_rotated(RotatedRect rect) {
     rect.points(rect_points);
     double h = lato(rect_points[0], rect_points[1]);
     double l = lato(rect_points[1], rect_points[2]);
-    int ret=l*h;
+    int ret = l * h;
     return ret;
 }
 
 RotatedRect find_bigger_rectangle(vector<vector<Point>> contours) {
-    RotatedRect minRect=minAreaRect(contours[0]);
-    int areaMax = area_rect_rotated(minRect);
+    RotatedRect minRect = minAreaRect(contours[0]);
     for (int i = 1;i < contours.size();i++) {
         RotatedRect rect_cont = minAreaRect(contours[i]);
-        int area_cont = area_rect_rotated(rect_cont);
-        if (areaMax < area_cont) {
-            areaMax = area_cont;
+        if (minRect.size.height+ minRect.size.width < rect_cont.size.height+ rect_cont.size.width) {
             minRect = rect_cont;
         }
     }
@@ -97,19 +97,28 @@ void find_rectangle() {
     Canny(card2, canny_output, thresh_min, thresh_max);
     vector<vector<Point> > contours;
     findContours(canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-    bigger_rect=find_bigger_rectangle(contours);
+    bigger_rect = find_bigger_rectangle(contours);
 }
 
+
+Mat copy_to_cropped_rectangle() {
+    Point starting_point;
+    starting_point.x = bigger_rect.center.x - bigger_rect.size.width / 2+1;
+    starting_point.y = bigger_rect.center.y - bigger_rect.size.height / 2+1;
+    Mat ret = card2(cv::Rect(starting_point.x, starting_point.y, bigger_rect.size.width, bigger_rect.size.height));
+
+    return ret;
+}
 
 Mat rotate_card(Mat img) {
     card2 = img;
     find_rectangle();
-    if(bigger_rect.angle){
-      Mat r = getRotationMatrix2D(bigger_rect.center, bigger_rect.angle, 1.0);
-      warpAffine(card2, card2, r, card2.size());
-  }
 
-    return card2;
+    Mat r = getRotationMatrix2D(bigger_rect.center, bigger_rect.angle, 1.0);
+    warpAffine(card2, card2, r, card2.size());
+    return copy_to_cropped_rectangle();
+
+
 }
 
 
