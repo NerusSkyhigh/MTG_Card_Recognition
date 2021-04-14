364abd5516f4299f0e119e14acc30f88f5a3b9f9
EttoreFavari <ettore.favari@unitn.it>
2021-04-14 11:05:11 +0200

    Aggiunto programma per aggiustare la rotazione della carta

--- programmi/adjust_rotation.cpp
+++ programmi/adjust_rotation.cpp
@@ -3,29 +3,29 @@
 #include "opencv2/highgui.hpp"
 #include <iostream>
 #include<cstring>
+#include<cmath>
 using namespace std;
 using namespace cv;
 
 
-const char* path_image = "C:/Users/User/Desktop/cards_db/Amonkhet/uncommon/7.jpg";
+//const char* path_image = "C:/Users/User/Desktop/cards_db/Amonkhet/uncommon/7.jpg";
+const char* path_image = "C:/Users/User/source/repos/LOOOL/x64/Debug/Storta.png";
 const char* input = "Card";
 const char* output = "output";
+
+const int thresh_min = 128;
+const int thresh_max = 256;
+Mat canny_output;
+RotatedRect bigger_rect;
 
 #define GUI true
 #define SHOWOUTPUT true
 #define SHOWINPUT true
 
-
-
-int thresh =127;
-
-
-
-
 Mat card2;
 
 
-Mat find_rotation(Mat img);
+Mat rotate_card(Mat img);
 
 
 int main(int argc, char* argv[]) {
@@ -48,7 +48,7 @@ int main(int argc, char* argv[]) {
             imshow(input, img);
         }
     }
-    Mat card_rotated = find_rotation(img);
+    Mat card_rotated = rotate_card(img);
     if (GUI) {
         if (SHOWOUTPUT) {
             namedWindow(output, WINDOW_AUTOSIZE);
@@ -59,26 +59,57 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+
+
+double lato(Point2f point1, Point2f point2) {
+    double x_2 = pow(point1.x-point2.x,2);
+    double y_2 = pow(point1.y-point2.y,2);
+    double ret = sqrt(x_2+y_2);
+    return ret;
+}
+
+
+
+int area_rect_rotated(RotatedRect rect) {
+    Point2f rect_points[4];
+    rect.points(rect_points);
+    double h = lato(rect_points[0], rect_points[1]);
+    double l = lato(rect_points[1], rect_points[2]);
+    int ret=l*h;
+    return ret;
+}
+
+RotatedRect find_bigger_rectangle(vector<vector<Point>> contours) {
+    RotatedRect minRect=minAreaRect(contours[0]);
+    int areaMax = area_rect_rotated(minRect);
+    for (int i = 1;i < contours.size();i++) {
+        RotatedRect rect_cont = minAreaRect(contours[i]);
+        int area_cont = area_rect_rotated(rect_cont);
+        if (areaMax < area_cont) {
+            areaMax = area_cont;
+            minRect = rect_cont;
+        }
+    }
+    return minRect;
+}
+
 void find_rectangle() {
-  Mat canny_output;
-  Canny( card2, canny_output, thresh, thresh*2 );
-  vector<vector<Point> > contours;
-  findContours( canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
+    Canny(card2, canny_output, thresh_min, thresh_max);
+    vector<vector<Point> > contours;
+    findContours(canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+    bigger_rect=find_bigger_rectangle(contours);
 }
 
 
-void calcolate_angle(){}
-Mat rotate_card() {
-    return Mat();
-}
-
-Mat find_rotation(Mat img) {
+Mat rotate_card(Mat img) {
     card2 = img;
     find_rectangle();
-    calcolate_angle();
+    if(bigger_rect.angle){
+      Mat r = getRotationMatrix2D(bigger_rect.center, bigger_rect.angle, 1.0);
+      warpAffine(card2, card2, r, card2.size());
+  }
 
-    return rotate_card();
+    return card2;
 }
 
 
