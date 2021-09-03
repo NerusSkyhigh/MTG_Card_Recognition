38a7e8f0086db144a5dd98faeb443b1adb7786de
nerussyhigh <Guglielmo.grillo@gmail.com>
2021-09-03 17:12:17 +0200

    introduce pretty printify for dictionaries, reintroduce updates and remove translations from the cards

--- MagicDatabaseDownloader/Main.py
+++ MagicDatabaseDownloader/Main.py
@@ -9,6 +9,12 @@ import os
 import logging
 import cv2
 
+from pprint import pprint
+
+import requests
+from PIL import Image
+import numpy as np
+img_url = "http://192.168.1.110:8080/photo.jpg"
 
 
 # Fase 1: Preparo il database
@@ -17,13 +23,14 @@ logging.basicConfig(level=logging.INFO)
 
 if ( not os.path.isfile("pickle/mtg_db_en.pkl") ):
     db:MTGJsonDatabase = MTGJsonDatabase.MTGJsonDatabase()
-    #db.Update()
+    db.Update()
     magic_sets = db.toDictonary()
 
     index = Index()
     # Add cards into the database
     for ms in magic_sets:
         for card in magic_sets[ms]['cards']:
+            card.pop("foreignData", None)
             if 'paper' in card['availability']:
                 flavorText = card['flavorText'] if 'flavorText' in card else ''
                 originalText = card['originalText'] if 'originalText' in card else ''
@@ -45,19 +52,35 @@ if ( not os.path.isfile("pickle/mtg_db_en.pkl") ):
         pickle.dump(index, f)
 
 else:
+    print("Apro il pickle")
     with open("pickle/mtg_db_en.pkl", "rb") as f:
         index = pickle.load(f)
+
+
+
+
 
 
 # Fase 2: Preparo il database
 while(True):
     input("Press Enter to continue...")
-    image = cv2.imread('test.jpg')
+
+
+    r = requests.get(img_url, stream=True)
+    image = np.asarray(bytearray(r.content), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    cv2.imshow('Photo', image)
+    cv2.waitKey()
+
+    #image = cv2.imread('test.jpg')
     image = Cropper.crop(image, grayscale=True)
     cv2.imwrite("output.png", image)
 
+
     tesseract_text = pytesseract.image_to_string(image)
     tesseract_text = str.rstrip(tesseract_text)
+
 
     query = index.standardize_keywords(tesseract_text)
 
@@ -69,4 +92,5 @@ while(True):
     if len(ranking) == 0:
         print("No card found")
     else:
-        print(index.get_card(ranking[0]) )
+        print("Name:", index.get_card(ranking[0])["name"] )
+        pprint(index.get_card(ranking[0]) )
